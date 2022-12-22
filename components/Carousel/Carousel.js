b7968dd8df78b0402494832d52647fe1f588d3df
Radu Cimpian <raducimpian@RaduCimpians-MacBook-Pro.local>
2022-12-22 16:41:56 +0200

    cleanup

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,7 @@
-import { useState } from 'react';
-import { IconButton, Flex } from '@chakra-ui/react';
-import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
-import { TechStackCard } from '../TechStackCard/TechStackCard';
-import { CAROUSEL_ITEM_TYPES } from '../../constants';
+import { useState } from "react";
+import { IconButton, Flex } from "@chakra-ui/react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import { CAROUSEL_ITEM_TYPES } from "../../constants";
 
 export const Carousel = ({ items = [], type = CAROUSEL_ITEM_TYPES.CARD }) => {
   const [currentFocusedIndex, setCurrentFocusedIndex] = useState(0);
@@ -22,28 +21,7 @@ export const Carousel = ({ items = [], type = CAROUSEL_ITEM_TYPES.CARD }) => {
       return (
         <>
           <Flex flexDirection="row" justifyContent="center" alignItems="center">
-            <IconButton
-              aria-label="left-arrow"
-              icon={<ChevronLeftIcon w={12} h={12} />}
-              size="lg"
-              onClick={onPressLeft}
-              disabled={currentFocusedIndex === 0}
-            />
-            {/* {items.map((item, index) => ( */}
-
-            <TechStackCard
-              cardTitle={items[currentFocusedIndex]?.title}
-              cardFooter={currentFocusedIndex}
-            />
-            {
-              // <TechStackCard cardTitle={item?.title} cardFooter={index} />;
-              // {
-              //   currentFocusedIndex !== 0 && (
-              //     <TechStackCard cardTitle={item?.title} cardFooter={index} />
-              //   );
-              // }
-              // ))}
-            }
+            <IconButton aria-label="left-arrow" icon={<ChevronLeftIcon w={12} h={12} />} size="lg" onClick={onPressLeft} disabled={currentFocusedIndex === 0} />
             <IconButton
               aria-label="right arrow"
               icon={<ChevronRightIcon w={12} h={12} />}
